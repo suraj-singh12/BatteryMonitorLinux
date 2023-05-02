bf2f3938a73d8605679e1dec4c1d34fac58ee284
suraj-singh12 <surajsingh5092@gmail.com>
2023-05-02 15:04:49 +0530

    updated with charging status

--- myBatteryMonitor.sh
+++ myBatteryMonitor.sh
@@ -4,21 +4,28 @@
 while true; do
     # Get the battery percentage as an integer
     battery_percentage=$(upower -i $(upower -e | grep BAT) | awk '/percentage:/ {sub(/%/, ""); printf "%d", $2}')
-
-    # Check if battery percentage is below 30
-    if [ "$battery_percentage" -lt 10 ]; then
+    # Get the charging status of the battery
+    charging=$(upower -i $(upower -e | grep BAT) | grep -q "state:\s*charging" && echo true || echo false)
+    
+    # Check if battery percentage is below 10 and "not charging"
+    if [ "$battery_percentage" -lt 10 ] && ! $charging; then
         # Battery is critically low
         echo -e "\e[31mBattery is critically low!\e[0m"
         echo -e "\e[31mShutting down the system in 1 min if not plugged in!\e[0m"
 
-        # Beep (assuming you have the "sox" command installed)
-        playu -q beep.wav repeat 1 > /dev/null 2>&1
+        # Beep alert
+        play -q beep.wav repeat 1 > /dev/null 2>&1
         
         # Wait for 60 seconds for the user to close everything or plug in charging
         seconds_left=60
         while [ $seconds_left -gt 0 ]; do
             # Clear the line and print the remaining seconds
             tput cuu1; tput el; echo -ne "\rShutting down in $seconds_left seconds"
+            
+            # Beep when last 10 seconds are remaining
+            if [ $seconds_left -le 10 ]; then
+                play -q beep_short.wav > /dev/null 2>&1
+            fi
             
             # Wait for 1 second
             sleep 1
@@ -29,6 +36,14 @@ while true; do
 
         # Shutdown the system
         shutdown now
+    fi
+    
+    # Check if battery percentage is greater than 95 and "charging"
+    if [ "$battery_percentage" -gt 95 ] && $charging; then
+        # Battery is fully charged
+        echo -e "\e[32mBattery is fully charged!\e[0m"
+        # Beep alert
+        play -q beep.wav > /dev/null 2>&1
     fi
 
     # Wait for 10 seconds before checking again
