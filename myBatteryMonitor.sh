4afacb66050669df25c1837b35045050b0a3b7ed
suraj-singh12 <surajsingh5092@gmail.com>
2023-05-07 10:39:39 +0530

    added improvements

--- myBatteryMonitor.sh
+++ myBatteryMonitor.sh
@@ -12,16 +12,31 @@ while true; do
     
     # Check if battery percentage is below 10 and "not charging"
     if [ "$battery_percentage" -lt 10 ] && ! $charging; then
+    	# clear the console first
+    	clear
+    	
         # Battery is critically low
         echo -e "\e[31mBattery is critically low!\e[0m"
         echo -e "\e[31mShutting down the system in 1 min if not plugged in!\e[0m"
 
         # Beep alert
         play -q $beep_file repeat 1 > /dev/null 2>&1
-        
+
+        sleep 10
+        clear
+
         # Wait for 60 seconds for the user to close everything or plug in charging
         seconds_left=60
         while [ $seconds_left -gt 0 ]; do
+            
+            # if at any time, system is plugged in charging, the timer should stop
+            currently_charging=$(upower -i $(upower -e | grep BAT) | grep -q "state:\s*charging" && echo true || echo false)
+            if $currently_charging; then
+                clear
+                echo 'Currently Charging'
+            	break;
+            fi
+            
             # Clear the line and print the remaining seconds
             tput cuu1; tput el; echo -ne "\rShutting down in $seconds_left seconds"
             
@@ -36,13 +51,14 @@ while true; do
             # Decrement the seconds left
             seconds_left=$((seconds_left - 1))
         done
-
-        # Shutdown the system
-        shutdown now
+        if [ $seconds_left -eq 0 ]; then
+            # Shutdown the system
+            shutdown now
+        fi
     fi
     
     # Check if battery percentage is greater than 95 and "charging"
-    if [ "$battery_percentage" -gt 95 ] && $charging; then
+    if [ "$battery_percentage" -gt 85 ] && $charging; then
         # Battery is fully charged
         echo -e "\e[32mBattery is fully charged!\e[0m"
         # Beep alert
